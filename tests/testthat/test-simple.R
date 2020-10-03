ffc74f0c9ebb46cd651e5eea506d3cd5d64e962f
rwdavies <robertwilliamdavies@gmail.com>
2020-10-03 12:55:41 +0100

    small stuff noticed just before or while recording  lecture

--- tests/testthat/test-simple.R
+++ tests/testthat/test-simple.R
@@ -3,7 +3,7 @@ test_that("can properly determine integer is odd using R", {
     expect_equal(R_is_odd(10L), FALSE)
     expect_equal(R_is_odd(11L), TRUE)
 })
-test_that("can properly determine integer is odd using R", {
+test_that("can properly determine integer is odd using Rcpp", {
     expect_equal(cpp_is_odd(10L), FALSE)
     expect_equal(cpp_is_odd(11L), TRUE)
 })
